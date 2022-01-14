b542b51b812b23c2ce0f3c3041e77720d1ca2e2c
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2022-01-14 23:56:14 +0800

    add

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Chart from 'kaktana-react-lightweight-charts'
 
 ReactDOM.render(
   <React.StrictMode>
